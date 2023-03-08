785df6e659499a36ef85164cab851d393f7d1628
elimorw <ivan.protska@yandex.ru>
2023-03-08 18:07:36 +0300

    UXAR1-1004_GATEWAY

---
+++ db-migration/src/main/java/ru/uxair1/dbservice/LiquibaseGuideApplication.java
@@ -0,0 +1,11 @@
+package ru.uxair1.dbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseGuideApplication.class, args);
+    }
+}
