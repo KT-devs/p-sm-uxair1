13c47dba4b56eafbdd103b2535d52db9303abef3
Evgeny Varlamov <varlamov-el@mail.ru>
2023-03-09 13:55:18 +0300

    UXAR1-1002 Eureka server creation

---
+++ eureka-server/src/test/java/ru/uxar1/eurekaserver/EurekaServerApplicationTest.java
@@ -0,0 +1,11 @@
+package ru.uxar1.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
