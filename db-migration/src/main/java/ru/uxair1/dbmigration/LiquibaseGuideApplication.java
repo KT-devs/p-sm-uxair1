1b6bbc534da9643782330b2346697a2184b9bc16
elimorw <ivan.protska@yandex.ru>
2023-03-12 00:53:32 +0300

    Version1

---
+++ db-migration/src/main/java/ru/uxair1/dbmigration/LiquibaseGuideApplication.java
@@ -0,0 +1,18 @@
+package ru.uxair1.dbmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LiquibaseGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseGuideApplication.class, args);
+    }
+    @Bean
+    public ReactiveWebServerFactory reactiveWebServerFactory() {
+        return new TomcatReactiveWebServerFactory();
+    }
+}
