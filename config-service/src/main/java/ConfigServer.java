994ea80302e80805a161b0e41a2339fe6a15ed3c
Max.D <veagud@mail.ru>
2023-03-18 16:46:25 +0300

    UXAR1-1003 Fix

---
+++ config-service/src/main/java/ConfigServer.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
