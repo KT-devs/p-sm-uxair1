3c8c70e51e2918567def35b10dc4c09aee473555
Max.D <veagud@mail.ru>
2023-03-21 22:08:32 +0300

    UXAR1-1037 Created enum FlightStatus fix

--- flight-service/src/main/java/ru/uxair/flight/entity/FlightStatus.java
+++ flight-service/src/main/java/ru/uxair/flight/entity/FlightStatus.java
@@ -1,5 +1,14 @@
 package ru.uxair.flight.entity;
 
 public enum FlightStatus {
-    SCHEDULED, ONTIME, DELAYED, DEPARTED, ARRIVED, CANCELED
+    SCHEDULED("Запланированный"), ONTIME("По расписанию"), DELAYED("Отложенный"), DEPARTED("Отправился"), ARRIVED("Прилетел"), CANCELED("Отменён");
+    private final String name;
+
+    FlightStatus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
