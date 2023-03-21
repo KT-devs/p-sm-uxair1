8fad413ce24aced393652674efe6b1dd56dc9e28
Evgeny Varlamov <varlamov-el@mail.ru>
2023-03-21 23:26:36 +0300

    UXAR1-1006 Passenger fix commit

--- passenger-service/src/main/java/ru/uxar1/passengerservice/passengerservice/repositiry/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.uxar1.passengerservice.passengerservice.repositiry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.uxar1.passengerservice.passengerservice.entity.Passenger;
-
-@Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
--- passenger-service/src/main/java/ru/uxar1/passengerservice/passengerservice/service/PassengerService.java
+++
@@ -1,9 +0,0 @@
-package ru.uxar1.passengerservice.passengerservice.service;
-
-import ru.uxar1.passengerservice.passengerservice.entity.Passenger;
-
-public interface PassengerService {
-    void savePassenger(Passenger passenger);
-    void updatePassenger(long id, Passenger updatedPassenger);
-    Passenger getPassenger(long id);
-}
