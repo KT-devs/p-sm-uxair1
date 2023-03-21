4f9d98af7ae965a32b2466b25bb65002e73ab935
Evgeny Varlamov <varlamov-el@mail.ru>
2023-03-21 22:44:05 +0300

    UXAR1-1027 Document fix

--- user-management/src/main/java/ru/uxair/user/entity/DocumentType.java
+++ user-management/src/main/java/ru/uxair/user/entity/DocumentType.java
@@ -1,5 +1,11 @@
 package ru.uxair.user.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DocumentType {
-    NATIONAL_PASSPORT, INTERNATIONAL_PASSPORT
+    NATIONAL_PASSPORT("NATIONAL_PASSPORT"), INTERNATIONAL_PASSPORT("INTERNATIONAL_PASSPORT");
+    private final String type;
 }
