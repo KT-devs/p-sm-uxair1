a0b7798b3b67c2dc3d782e3d506f6cffa9edc404
MusPati <murpati36@gmail.com>
2023-03-21 22:57:48 +0500

    test

---
+++ authorization-service/src/main/java/ru/uxair/authorization/controller/TestControler.java
@@ -0,0 +1,16 @@
+package ru.uxair.authorization.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestControler {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello, Chikago!");
+    }
+}
---
+++ authorization-service/src/main/java/ru/uxair/authorization/controller/security/RegisterRequest.java
@@ -0,0 +1,25 @@
+package ru.uxair.authorization.controller.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.uxair.authorization.entity.Role;
+
+import java.util.Date;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Date dateRegistration;
+    private String password;
+    private Role role;
+
+}
