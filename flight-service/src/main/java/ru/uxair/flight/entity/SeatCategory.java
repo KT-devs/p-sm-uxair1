b2906ac2852c9dcb20d9d386528ea7a573c6a129
MusPati <murpati36@gmail.com>
2023-03-23 23:04:08 +0500

    created enum SeatCategory

---
+++ flight-service/src/main/java/ru/uxair/flight/entity/SeatCategory.java
@@ -0,0 +1,16 @@
+package ru.uxair.flight.entity;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SeatCategory {
+
+        FIRST("First"),
+        BUSINESS("Business"),
+        ECONOMY("Economy");
+
+        private final String category;
+}
