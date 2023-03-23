0317b806bb4cb103576c5f2c103260fa713b39b2
Sergey Melnikov <74730932+pdasilem@users.noreply.github.com>
2023-03-23 15:13:21 +0400

    fixpath (#25)

--- db-migration/src/main/java/ru/uxair/dbmigration/LiquibaseGuideApplication.java
+++ db-migration/src/main/java/ru/uxair/dbmigration/LiquibaseGuideApplication.java
@@ -1,10 +1,7 @@
-package ru.uxair1.dbmigration;
+package ru.uxair.dbmigration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatReactiveWebServerFactory;
-import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiquibaseGuideApplication {
