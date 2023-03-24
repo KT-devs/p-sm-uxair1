6cc56ac4833633a40d073c2587a3475eb0ea3d64
Max.D <veagud@mail.ru>
2023-03-24 10:45:15 +0300

    UXAR1-1012  enum fix

--- flight-service/src/main/java/ru/uxair/flight/entity/SeatCategory.java
+++ flight-service/src/main/java/ru/uxair/flight/entity/SeatCategory.java
@@ -1,8 +1,15 @@
 package ru.uxair.flight.entity;
 
 public enum SeatCategory {
-    FIRST,
-    BUSINESS,
-    ECONOMY
+    FIRST("Первый класс"), BUSINESS("Бизнес класс"), ECONOMY("Эконом класс");
+    private final String name;
+
+    SeatCategory(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
 
