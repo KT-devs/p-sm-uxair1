d2c6b6e4a5248d088069d316116be00b7f8339d7
MusPati <murpati36@gmail.com>
2023-03-29 01:20:00 +0500

    1

--- db-migration/src/main/java/ru/uxair/dbmigration/LiquibaseGuideApplication.java
+++ db-migration/src/main/java/ru/uxair/dbmigration/LiquibaseGuideApplication.java
@@ -2,11 +2,15 @@ package ru.uxair.dbmigration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiquibaseGuideApplication {
     public static void main(String[] args) {
         SpringApplication.run(LiquibaseGuideApplication.class, args);
     }
+
 
 }
