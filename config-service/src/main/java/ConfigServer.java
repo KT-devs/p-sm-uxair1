092c9b3ae45d43e6c508fe35d67230b58d549ad6
Max.D <veagud@mail.ru>
2023-03-31 10:31:03 +0300

    UXAIR-1003

--- config-service/src/main/java/ConfigServer.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.boot.SpringApplication;
-
-public class ConfigServer {
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class, args);
-    }
-}
